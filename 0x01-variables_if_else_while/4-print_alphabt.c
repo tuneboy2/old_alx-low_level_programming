97ad3d3987e99250a571addf76a2fcc902c55c0d
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-12 00:25:31 +0100

    Exception of Letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
-
+/**
+* main - void
+*
+* Description: This is the entry point for a C program
+* Return: 0
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+if (c == 'q', 'e')
+continue;
+}
+putchar('\n');
+return (0);
+}
