b9c4522b3bd849c1a373f95b2daa395b52d8e155
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-11 16:29:24 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /*
-* main - void
+* main - void 
+h
 * Description: This is the main entry of the C program
 * printf - prints tge argument to the standard output
 * Return: returns an integer to the whomever that called
