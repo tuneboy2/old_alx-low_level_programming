7f3fb48691005f246db66ea572e1f06a2f407bf2
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-11 23:55:52 +0100

    Print Mixed Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-
+/**
+* main - void
+*
+* Description: This is the entry point of a C program
+* Return: 0
+*/
+int main(void)
+{
+char c = 'a';
+char O = 'A';
+while (c <= z)
+{
+putchar(c);
+c++;
+}
+while (O <= 'Z')
+{
+putchar(O);
+O++;
+}
+putchar('\n')
+return (0)
+}  
