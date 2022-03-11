c96a00c434f7fd51e6206844ce4370f7200838a7
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-11 15:54:57 +0100

    Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
 #include <studio.h>
+/** main - this is tge the entry point of the C program
+* printf - prints tge argument to the standard output
+* Return: returns an integer to the whomever that called
+*/
 int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n');
+return (0)
+}
