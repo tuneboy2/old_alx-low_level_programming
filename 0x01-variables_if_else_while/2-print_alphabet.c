5454a4b1583e8d20ade57d221ae9b618b8cfd14d
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-11 22:47:11 +0100

    Print Lowercase Letters

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
 * main - entry point
-* 
+*
 * Description: This is the entry point of the C program
 * Return: 0
 */
-int main()
+int main(void)
 {
-char b = 'a' 
+char b = 'a'
 while (b < 123)
-putchar(b); b++;
-return 0
+putchar(b);
+b++;
+return (0)
 }
