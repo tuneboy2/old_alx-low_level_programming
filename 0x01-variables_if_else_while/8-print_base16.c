ee98f8ad5db7ed2e3d417f428a8150e043099717
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-12 02:47:17 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+/**
+* main - void
+*
+* Description: print a hexadecimal number
+* Return: 0
+*/
+int main(void)
+{
+int b = 48;
+while (b < 58)
+{
+putchar(b);
+b++;
+}
+char d = 'a';
+char max = 'g';
+while (d < max)
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
