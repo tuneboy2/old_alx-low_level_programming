f2a6dd9862bc63eee82b7574ee29249570c1992a
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-12 01:36:45 +0100

    Print Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
-
+/**
+* main -void
+*
+* Description: This is the entry point of the C program
+* Return: 0
+*/
+int main(void)
+{
+int b = 0;
+while (b < 10)
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return (0);
+}
