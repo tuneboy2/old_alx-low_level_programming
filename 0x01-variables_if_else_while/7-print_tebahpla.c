1879a1d480f1fbf39d34f9729a580074d48878e4
tuneboy2 <99305529+tuneboy2@users.noreply.github.com>
2022-03-12 02:20:53 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+/**
+* main - void
+*
+* Description: Prints the alphabet in reverse order
+* Return: 0
+*/
+int main(void)
+{
+  char b = 'z';
+  while (b => 'a')
+    {
+    putchar(b);
+    b--;
+    }
+  putchar('\n');
+  return (0);
+}
+  
