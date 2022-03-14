425200e6ac2fd254b3b17d1b9192b0d8ed5adee7
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-14 16:54:40 -0700

    Using function putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - void
+*
+* Description: Write a program that prints pos*sible of single-digit numbers
+* Return: 0 - Success
+*/
+int main(void)
+{
+int x = 0;
+while
+(x <= 9);
+{
+putchar(x);
+x++;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
