f1c8003db8abc17ac996b1ae94b058d5bca9774d
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-14 16:01:01 -0700

    Printing Statements with Put function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*
+* main - void
+*
+* Description: Print a statement with tge function puts().
+* Return: 0 = Success
+*/
+
+int main(void){
+puts("Programming is like building a multilingual puzzle");
+
+return (0);
+}
