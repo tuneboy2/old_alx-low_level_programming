e783059b72fd933b510054f364e3db9b055b5b74
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-14 17:42:00 -0700

    Combination of digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,15 @@
 int main(void)
 {
 int x = 0;
-while
-(x <= 9);
+while (x < 10)
 {
-putchar(x);
-x++;
+putchar(x + '0');
+if (x < 9)
+{
 putchar(',');
 putchar(' ');
+}
+x++;
 }
 putchar('\n');
 return (0);
