59579c49715c2b83730eaa88e06bba52c18ccc9b
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-14 21:21:49 -0700

    Combimation of Three Digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+/**
+* main - void
+*
+* Description: A progtam that prints all possible
+* combinations of three digits
+* Return: 0 - Success
+*/
+int main(void)
+{
+int a, b, c;
+a = 48;
+b = 48;
+c = 48;
+while (a < 58)
+{
+b = b + 1;
+while (b < 58)
+{
+c = a + 1;
+while (c < 58)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a < 55 || b < 56 || c < 57)
+{
+putchar(',');
+putchar(' ');
+}
+c++;
+}
+b++;
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
