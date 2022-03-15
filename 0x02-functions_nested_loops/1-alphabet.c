a83db6d4b0acfa92cc737d2c5be565571b54f126
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-15 08:27:38 -0700

    Using functions to print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - void
+*
+* Description: A function thst prints the alphabet* in lowercase
+* Return: 0 - SUCCESS
+*/
+int main(void)
+{
+char a = 97;
+while (a < 123)
+{
+_putchar(a);
+a++;
+}
+_putchar(10);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+    print_alphabet();
+    return;
+}
