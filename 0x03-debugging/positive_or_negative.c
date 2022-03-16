aa7cd94380e6c763698b1a9d1822987a9ec12908
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-16 05:50:50 -0700

    Test Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* positive_or_negative - void
+* @i: The integer used when the function is called
+*
+* Description: return an integer 0 to whom called
+* it
+* Return: return 0 (Success)
+*/
+void positive_or_negative(int i)
+{
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+}
