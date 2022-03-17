3cb84ecb231397888898fe0a59d94a96f2a4ebcb
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-17 04:42:13 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_numbers - The function that's called
+*
+* Description: A function that prints the numbers
+* Return: void
+*/
+
+void print_numbers(void)
+{
+
+int l = 48;
+
+while (l < 58)
+{
+_putchar(l);
+l++;
+}
+
+_putchar(10);
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,7 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+void print_numbers(void);
+
+int _putchar(char c);
+
