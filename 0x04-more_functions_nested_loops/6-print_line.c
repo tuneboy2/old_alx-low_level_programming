6b0bbbffce0ecd076e322b36aaaf778f28ef7959
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-17 16:37:34 -0700

    Lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_line - The called function
+* @n: The parameter used when the function is called
+*
+* Description: A function that draws a straight line
+* Return: void
+*/
+
+void print_line(int n)
+{
+int c;
+if (n <= 0)
+{
+_putchar(10);
+}
+
+else
+{
+for (c = 0; c < n; c++)
+_putchar(95);
+}
+
+_putchar(10);
+}
