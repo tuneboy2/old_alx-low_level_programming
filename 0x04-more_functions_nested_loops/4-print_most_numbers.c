c353e60667e6792c234c595f4e50077468a6f31d
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-17 06:03:26 -0700

    Print with an Exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_most_numbers - The function that's called
+*
+* Description: A function that prints number but
+* with an exception.
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+
+int a;
+
+for (a = 48; a < 58; a++)
+{
+
+if (a == 52 || a == 54)
+{
+continue;
+}
+
+else
+{
+_putchar(a);
+}
+
+}
+_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,5 @@ void print_numbers(void);
 
 int _putchar(char c);
 
+void print_most_numbers(void);
+
