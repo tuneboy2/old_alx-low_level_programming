09e7f345095a743d7d54c0bcd4332fe58f8fbfb9
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-17 04:26:11 -0700

    Miltiplication of digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* mul - The function thats called
+* @a: The integer used when the function is called
+* @b: Second integer used
+*
+* Description: A function that multiplies integers
+* Return: 0 - SUCCESS
+*/
+
+int mul(int a, int b)
+{
+
+int sum;
+
+sum = (a * b);
+
+return (sum);
+
+}
