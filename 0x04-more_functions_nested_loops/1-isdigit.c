0378c6a818cfacde4e294897fb3fc3c270c85c59
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-17 04:12:32 -0700

    Digit Identifier

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _isdigit - The function thats called
+* @c: The integer used when the function is called
+*
+* Description: A function that identifies digit
+* integer
+* Return: 0 - SUCCESS
+*/
+
+int _isdigit(int c)
+{
+
+if (c > 47 && c < 58)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
