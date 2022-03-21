d9c1317b8d13e164285dde6f0dfe04fa365432a8
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-21 15:13:16 -0700

    Print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* puts_half - The function called
+* @str: The parameter used when called
+*
+* Description: Print half of a string
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int length, n;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+	length++;
+	}
+
+	if ((length % 2) == 0)
+	{
+	for (n = length / 2; str[n] != '\0'; n++)
+	{
+	_putchar(str[n]);
+	}
+	}
+	else if ((length % 2) != 0)
+	{
+	for (n = (length - 1) / 2; str[n] != '\0'; n++)
+	{
+	_putchar(str[n]);
+	}
+	}
+_putchar(10);
+}
