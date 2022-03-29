ab56cc930e9c12083903aa130eeecddd1a385c3f
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-28 17:57:47 -0700

    g

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,2 +1,11 @@
 char *_memset(char *s, char b, unsigned int n);
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+void print_diagsums(int *a, int size);
+
+void print_chessboard(char (*a)[8]);
+
+int _putchar(char c);
+
