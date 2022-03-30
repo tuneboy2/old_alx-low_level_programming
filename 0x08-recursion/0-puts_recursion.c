ef272d998385f1adf497bdee796c3392f91f7f47
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-03-30 05:20:40 -0700

    Recursion with strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,15 +10,12 @@
 
 void _puts_recursion(char *s)
 {
+	_putchar(*s);
 	if (*s == '\0')
 	{
+		_putchar(10);
 		return;
 	}
-
-	_putchar(*s);
 	s++;
 	_puts_recursion(s);
-
-	if (*s == '\0')
-	_putchar(10);
 }
