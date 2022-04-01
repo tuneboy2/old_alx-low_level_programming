a42b0ba12b981290f3ba7870709d3e57378868c2
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-01 15:35:58 -0700

    Argument Count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - A function that accepts arguments as parameters
+* @argc: Parameter that counts the number of arguments passed
+* @argv: Parameter that contains a string of arrays
+*
+* Return: 0 - SUCCESS
+*/
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%i\n", argc);
+
+return (0);
+}
