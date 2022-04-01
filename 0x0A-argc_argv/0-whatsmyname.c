613b52c5f4d6e4259908fd372fe74584ba3abda0
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-01 15:06:56 -0700

    Program Printing

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - The main function which accepts an argument
+* as parameters
+* @argc: A parameter that counts the number of arguments passed
+* @argv: A parameter that contains a string of arrays
+*
+* Return: 0 -SUCCESS
+*/
+
+int main(int argc, char* argv[])
+{
+	int i = 0;
+	
+	if (i < argc)
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
