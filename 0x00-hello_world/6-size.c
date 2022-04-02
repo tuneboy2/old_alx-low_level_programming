ebf3af27f7775f74767d3ae5f8bfb7314726541a
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-02 13:41:05 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,5 +9,6 @@
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char);
-printf("Size of an int: %d byte(s)\n", sizeof(int(
-printf("Size of a long int: %d byte(s)\n", sizeof
+printf("Size of an int: %d byte(s)\n", sizeof(int);
+printf("Size of a long int: %d byte(s)\n", sizeof);
+}
