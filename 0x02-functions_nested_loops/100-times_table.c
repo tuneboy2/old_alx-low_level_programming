3e664182b15a616de90c966bbc2bf7154f76e4a6
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-07 14:05:16 -0700

    Documentation Update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,13 +11,16 @@ void print_times_table(int n)
 {
 	int i, j, mul;
 
-if (n > 0 && n < 15)
+if (n >= 0 && n < 15)
 {
 	for (i = 0; i <= n; i++)
 	{
+	if (n != 0)
+	{
 	_putchar('0');
 	_putchar(',');
-		for (j = 1; j <= n; j++)
+	}
+	for (j = 1; j <= n; j++)
 		{
 			mul = i * j;
 			if (mul > 9 && mul < 100)
