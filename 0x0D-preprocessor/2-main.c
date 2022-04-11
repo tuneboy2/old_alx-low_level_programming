40ce12b2c9d96e30db62c33ed18f6a494c3d1851
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-11 14:43:34 -0700

    Documentation Updated

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - This prgrams prints the name of the program
+* it was compiled from
+*
+* Return: 0 - SUCCESS
+*/
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+
+	return(0);
+}
