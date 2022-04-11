4f9b6ad3bca8054ee81c3d985767d0d0774756c2
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-10 17:51:07 -0700

    Documentation Updated

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,37 +10,20 @@
 
 void print_number(int n)
 {
+	unsigned int num;
 
-if (n >= 10 && n < 100)
-{
-_putchar(n / 10 + '0');
-_putchar(n % 10 + '0');
-}
+	if (n > 0)
+		num = n;
 
-else if (n >= 100 && n < 1000)
-{
-_putchar(n / 100 + '0');
-_putchar((n % 100) / 10 + '0');
-_putchar((n % 100) % 10 + '0');
-}
+	else
+	{
+		num = -1 * n;
+		_putchar('-');
+	}
 
-else if (n >= 1000 && n < 10000)
-{
-_putchar(n / 1000 + '0');
-_putchar((n / 1000) / 100 + '0');
-_putchar((((n / 1000) / 100) / 10) + '0');
-_putchar((((n / 1000) / 100) % 10) + '0');
-}
+	if (num / 10)
+		print_number(num / 10);
 
-else if (n < 0 && n > -100)
-{
-_putchar(((n * -1) / 10) * -1 + '0');
-_putchar((n * -1) % 10 + '0');
-}
-else
-{
-_putchar(n + '0');
-}
-
+	_putchar(num % 10 + '0');
 return;
 }
