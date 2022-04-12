907282a8f904e69edc36cbc78a4fefe772b9fb6e
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-11 17:17:14 -0700

    Documentation Updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+* struct dog - The structure data type for dog
+* @name: Parameter 1
+* @age: Age of the dog
+* @owner: Owner of the dog parameter
+*
+* Description: Structure data typ
+*/
 
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+#endif
