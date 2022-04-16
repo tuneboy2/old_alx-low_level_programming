3bc74c8852128cc8177ff0525aaa0ea9a0d90762
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-16 04:34:47 -0700

    Doc

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* cap_string - A function that capitalizes all words of a string.
+* @n: The parameter of a pointer to a string.
+*
+* Return: A pointer to a character array
+*/
+
+char *cap_string(char *n)
+{
+	int i;
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] == ' ' || n[i] == '\t' || n[i] == '\n' || n[i] == ',' || n[i] == ';' || n[i] == '.'|| n[i] == '!' || n[i] == '?' || n[i] == '"')
+		{
+			if (*(n + i + 1) > 96 && *(n + i + 1) < 123)
+			*(n + i + 1) -= 32;
+		}
+	}
+return (n);
+}
