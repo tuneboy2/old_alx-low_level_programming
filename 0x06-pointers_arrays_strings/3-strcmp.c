6eba5eab22d05db4e123fe7ba23f3481da14ae3a
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-18 03:28:43 -0700

    String Compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strcmp - Write a function that compares two strings
+* @s1: Parameter 1
+* @s2: Parameter 2
+*
+* Return: An Integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, ans;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1 != s2)
+		{
+			if (s1[i] == s2[i])
+				continue;
+
+		else
+			{
+				ans = s1[i] - s2[i];
+				break;
+			}
+		}
+
+		else
+			ans = 0;
+	}
+return (ans);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
+int _strcmp(char *s1, char *s2);
+
 char *cap_string(char *);
