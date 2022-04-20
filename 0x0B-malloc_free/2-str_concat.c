312181e679fea31bb0c0e023359036e552fc71aa
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-20 08:03:56 -0700

    h

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,15 @@ char *str_concat(char *s1, char *s2)
 {
 	int b, i, j, k, l;
 	char *new;
-
-	if (s1 == NULL)
-		s1[0] = '\0';
 	
-	if (s2 == NULL)
-		s2[0] = '\0';
+	while (1)
+	{
+		if (s1 == NULL)
+			s1[0] = '\0';
+	
+		else if (s2 == NULL)
+			s2[0] = '\0';
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
