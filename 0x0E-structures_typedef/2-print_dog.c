f010f10b065485873ee182121b1ab76bad0baf2e
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-21 09:18:30 -0700

    Print Struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,34 +14,14 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		if (d->name == NULL)
-			printf("Name: (nil)\nAge: %3.6f\nOwner: %s\n", d->age, d->owner);
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
-		else if (d->owner == NULL)
-			printf("Name: %s\nAge: %3.6f\nOwner: (nil)\n", (*d).name, d->age);
+		printf("Age: %f\n", d->age);
 
-		else if (d->name == NULL && d->owner == NULL)
-		printf("Name: (nil)\nAge: %3.6f\nOwner: (nil)\n", d->age);
-
-	printf("Name: %s\nAge: %3.6f\nOwner: %s\n", (*d).name, d->age, d->owner);
-	}
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);	}
 }
-
-/*
-*void print_dog(struct dog *d)
-*{
-*	if (d)
-*	{
-*		if (!(d->name))
-*			printf("Name: (nil)\n");
-*		else
-*			printf("Name: %s\n", d->name);
-*
-*		printf("Age: %f\n", d->age);
-*
-*		if (!(d->owner))
-*			printf("Owner: (nil)\n");
-*		else
-*			printf("Owner: %s\n", d->owner);
-*	}
-*}
-*/
