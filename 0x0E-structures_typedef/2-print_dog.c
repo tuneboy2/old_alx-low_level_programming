60d150212114925052a3934728b48985dc0fac99
Oladapo Olatunbosun <mymailday1@gmail.com>
2022-04-21 09:03:48 -0700

    Print Struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,29 @@ void print_dog(struct dog *d)
 		else if (d->owner == NULL)
 			printf("Name: %s\nAge: %3.6f\nOwner: (nil)\n", (*d).name, d->age);
 
-		else
-			printf("Name: %s\nAge: %3.6f\nOwner: %s\n", (*d).name, d->age, d->owner);
+		else if (d->name == NULL && d->owner == NULL)
+		printf("Name: (nil)\nAge: %3.6f\nOwner: (nil)\n", d->age);
+
+	printf("Name: %s\nAge: %3.6f\nOwner: %s\n", (*d).name, d->age, d->owner);
 	}
 }
+
+/*
+*void print_dog(struct dog *d)
+*{
+*	if (d)
+*	{
+*		if (!(d->name))
+*			printf("Name: (nil)\n");
+*		else
+*			printf("Name: %s\n", d->name);
+*
+*		printf("Age: %f\n", d->age);
+*
+*		if (!(d->owner))
+*			printf("Owner: (nil)\n");
+*		else
+*			printf("Owner: %s\n", d->owner);
+*	}
+*}
+*/
